1623571710843d04f49b240b4cd5f82c6063f54d
AfsarMammadov <afsarmammadli@gmail.com>
2020-07-04 02:22:41 +0400

    first commit

---
+++ app/src/main/java/com/example/aquality/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.aquality;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    private TextView registerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        registerButton=findViewById(R.id.register);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
