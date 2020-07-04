ce92af5b26bba6fe1b65ea421af865248043438c
AfsarMammadov <afsarmammadli@gmail.com>
2020-07-04 20:38:18 +0400

    Updated Project

--- app/src/main/java/com/example/aquality/RegisterActivity.java
+++ app/src/main/java/com/example/aquality/RegisterActivity.java
@@ -2,13 +2,28 @@ package com.example.aquality;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Space;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class RegisterActivity extends AppCompatActivity {
+    private Spinner region;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        region=findViewById(R.id.region);
+        ArrayList<String> regions=new ArrayList<>();
+        regions.add(0,"Bölgə seçin");
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,regions);
+        region.setAdapter(adapter);
+
     }
 }
