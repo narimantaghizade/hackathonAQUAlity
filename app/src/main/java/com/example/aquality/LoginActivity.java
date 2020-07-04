434f7607f7afb8e0d594c896013b147a20cd3080
narimantaghizade <neriman.taghizade.99@gmail.com>
2020-07-04 04:12:58 +0400

    second commit

--- app/src/main/java/com/example/aquality/LoginActivity.java
+++ app/src/main/java/com/example/aquality/LoginActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private TextView registerButton;
 
     @Override
@@ -16,7 +19,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         registerButton=findViewById(R.id.register);
-
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,5 +26,11 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void launchMainActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
