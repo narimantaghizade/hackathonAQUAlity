e2a9a9373d5dbf6aa48bfdf652eb6ce2e2df3b15
narimantaghizade <neriman.taghizade.99@gmail.com>
2020-07-05 16:16:38 +0400

    api commit

--- app/src/main/java/com/example/aquality/LoginActivity.java
+++ app/src/main/java/com/example/aquality/LoginActivity.java
@@ -1,23 +1,38 @@
 package com.example.aquality;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private TextView registerButton;
+    private Button button_login_login;
+    private EditText editText_login_username;
+    private EditText editText_login_password;
+    private String email;
+    private String password;
+    private String baseUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        baseUrl = "https://aquality.herokuapp.com/api/login";
+        editText_login_username = (EditText) findViewById(R.id.email);
+        editText_login_password = (EditText) findViewById(R.id.password);
+
+        button_login_login = (Button) findViewById(R.id.login_in);
+
+
+
         registerButton=findViewById(R.id.register);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,11 +41,72 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 
-    public void launchMainActivity(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
+    /**
+     * This subclass handles the network operations in a new thread.
+     * It starts the progress bar, makes the API call, and ends the progress bar.
+     */
+    public class ExecuteNetworkOperation extends AsyncTask<Void, Void, String> {
+
+        private ApiAuthenticationClient apiAuthenticationClient;
+        private String isValidCredentials;
+
+        /**
+         * Overload the constructor to pass objects to this class.
+         */
+        public ExecuteNetworkOperation(ApiAuthenticationClient apiAuthenticationClient) {
+            this.apiAuthenticationClient = apiAuthenticationClient;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            // Display the progress bar.
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                isValidCredentials = apiAuthenticationClient.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            // Hide the progress bar.
+
+            // Login Success
+            if (isValidCredentials.equals("true")) {
+
+            }
+            // Login Failure
+            else {
+                Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+
+
+
+    public void launchMainActivity(View V) {
+        Bundle bundle = new Bundle();
+        bundle.putString("username", email);
+        bundle.putString("password", password);
+        bundle.putString("baseUrl", baseUrl);
+
         Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtras(bundle);
         startActivity(intent);
     }
 }
